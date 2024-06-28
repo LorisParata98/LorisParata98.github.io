b499edaa45100a4041a9b0af4cbcb2189036ca11
Loris Parata <lorisparata@gmail.com>
2024-06-28 17:35:09 +0200

    feat() add public

---
+++ my-site/public/my-site/server/chunk-Z2WRUHLT.mjs
@@ -0,0 +1,3 @@
+import './polyfills.server.mjs';
+import{a as e,b as i,c as p,j as n,l as m,m as s,p as f,v as a}from"./chunk-I2P6GIHB.mjs";var c=(()=>{let o=class o{constructor(){this.title="my-site"}};o.\u0275fac=function(r){return new(r||o)},o.\u0275cmp=e({type:o,selectors:[["app-root"]],standalone:!0,features:[p],decls:1,vars:0,template:function(r,x){r&1&&i(0,`Salve
+`)}});let t=o;return t})();var d=[];var l={providers:[a(d),s()]};var g={providers:[f()]},u=n(l,g);var C=()=>m(c,u),k=C;export{k as a};
---
+++ my-site/public/my-site/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-Z2WRUHLT.mjs";import"./chunk-I2P6GIHB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
