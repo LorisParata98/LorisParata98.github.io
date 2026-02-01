29dc8729ce059daecc9c0ef2cc9807fffb72a384
Loris Parata <33716296+LorisParata98@users.noreply.github.com>
2026-02-01 23:24:13 +0100

    add ignore

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    firebase: {
-        apiKey: "AIzaSyDRjL2bG-DnSkk2r664NtScPkqX6CNxatw",
-        authDomain: "test-pwa-d51a4.firebaseapp.com",
-        projectId: "test-pwa-d51a4",
-        storageBucket: "test-pwa-d51a4.firebasestorage.app",
-        messagingSenderId: "769749607138",
-        appId: "1:769749607138:web:6badde27a31a69c9f6b4bb"
-    }
-}
+  firebase: {
+    apiKey: 'AIzaSyDRjL2bG-DnSkk2r664NtScPkqX6CNxatw',
+    authDomain: 'test-pwa-d51a4.firebaseapp.com',
+    projectId: 'test-pwa-d51a4',
+    storageBucket: 'test-pwa-d51a4.firebasestorage.app',
+    messagingSenderId: '769749607138',
+    appId: '1:769749607138:web:6badde27a31a69c9f6b4bb',
+  },
+};
