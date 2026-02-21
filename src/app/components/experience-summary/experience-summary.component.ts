a9bca0bedc6e3eb9abd5a740c18e3eff8f128314
Loris Parata <33716296+LorisParata98@users.noreply.github.com>
2026-02-21 19:32:08 +0100

    fix styling

--- src/app/components/experience-summary/experience-summary.component.ts
+++ src/app/components/experience-summary/experience-summary.component.ts
@@ -9,4 +9,16 @@ import { CompletationBarComponent } from './completation-bar/completation-bar.co
   templateUrl: './experience-summary.component.html',
   styleUrl: './experience-summary.component.scss',
 })
-export class ExperienceSummaryComponent {}
+export class ExperienceSummaryComponent {
+  public expirenceYears: number;
+  constructor() {
+    this.expirenceYears = this.calculateExperienceYears();
+  }
+  calculateExperienceYears(): any {
+    const startDate = new Date(2018, 7, 31); // Agosto 1, 2018
+    const currentDate = new Date();
+    const diffInMs = currentDate.getTime() - startDate.getTime();
+    const diffInYears = diffInMs / (1000 * 60 * 60 * 24 * 365.25);
+    return Math.floor(diffInYears);
+  }
+}
